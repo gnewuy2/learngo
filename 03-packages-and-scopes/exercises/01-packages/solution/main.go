73e87e8a6feed53e3150ad9b0c466fd42c2cc0a9
Weng <Yu.Weng@cbre.com>
2019-10-13 21:21:53 -0500

    add main.go file to reference greet() and bye() from the same package and folder

--- 03-packages-and-scopes/exercises/01-packages/solution/main.go
+++ 03-packages-and-scopes/exercises/01-packages/solution/main.go
@@ -3,7 +3,7 @@
 // Copyright © 2018 Inanc Gumus
 // Learn Go Programming Course
 // License: https://creativecommons.org/licenses/by-nc-sa/4.0/
-//
+// To run this in the terminal: go run .  or go run main.go greet.go bye.go
 
 package main
 
